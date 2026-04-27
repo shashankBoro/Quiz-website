6c226966e6f21e2baab0094023ab4c0603e9a584
shashankBoro <shashankboro179@gmail.com>
2026-04-27 20:22:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Section Navigation
+function showSection(id) {
+    document.querySelectorAll('.section').forEach(sec => {
+        sec.classList.remove('active');
+    });
+
+    document.getElementById(id).classList.add('active');
+}
